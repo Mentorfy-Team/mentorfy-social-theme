df32abb6a36b4ac9a11dfcb017007f96945b7f1d
Alan Jones Rios <alan.jones.rios@gmail.com>
2025-01-09 12:58:39 -0300

    🔧 chore: simplificado código de auto-resposta •⁠  ⁠removidos logs desnecessários para limpeza do código. •⁠  ⁠ajustado o tempo de espera para abrir o compositor. •⁠  ⁠mantida a lógica de abertura do compositor ao carregar tópicos com parâmetro 'reply'.

--- javascripts/discourse/api-initializers/auto-reply.js
+++ javascripts/discourse/api-initializers/auto-reply.js
@@ -6,28 +6,24 @@ export default {
   initialize() {
     withPluginApi("0.8.31", (api) => {
       api.onAppEvent('page:topic-loaded', (data) => {
-        console.log("👆 page:topic-loaded", window.location, data);
-
         setTimeout(() => {
-            console.log("👆 page:topic-loaded timeout", window.location, data);
-        }, 2000)
-
-        if (new URLSearchParams(window.location.search).get('reply') === 'true') {
-          const composer = api.container.lookup("controller:composer");
-          const topicController = api.container.lookup("controller:topic");
-
-          if (topicController) {
-            const topic = topicController.get("model");
-            if (topic) {
-              composer.open({
-                action: REPLY,
-                draftKey: topic.draft_key,
-                draftSequence: topic.draft_sequence,
-                topic,
-              });
-            }
-          }
-        }
+            if (new URLSearchParams(window.location.search).get('reply') === 'true') {
+                const composer = api.container.lookup("controller:composer");
+                const topicController = api.container.lookup("controller:topic");
+      
+                if (topicController) {
+                  const topic = topicController.get("model");
+                  if (topic) {
+                    composer.open({
+                      action: REPLY,
+                      draftKey: topic.draft_key,
+                      draftSequence: topic.draft_sequence,
+                      topic,
+                    });
+                  }
+                }
+              }
+        }, 1000)
       });
     });
   },
